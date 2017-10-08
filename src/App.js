2f06a662b98623869e0c43ba6eaf76687fc62d02
Rohan Malhotra <rohan.root@gmail.com>
2017-10-08 12:48:29 +0530

    added blend anime

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import axios from "axios";
 import Webcam from "./react-webcam";
 import { base64ToBlob } from "./utils";
 import sound from "./song.mp3"
+import randomColor from "random-color"
 import "./styles.css";
+
+// 60*136/1000
+const pulse = 400
 
 const style = {
   webcamWrapper: {
@@ -31,7 +35,8 @@ class App extends Component {
         data: {},
         fetching: false,
         error: ""
-      }
+      },
+      color: "transparent"
     };
   }
 
@@ -75,6 +80,9 @@ class App extends Component {
         const apiKey = "621280fd2b3f5d4ffcf98dc31920ccecd1b79d7c";
         const url = `https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=${apiKey}&version=2016-05-20`;
         this.setState({ food: { ...this.state.food, fetching: true } });
+        this.colorChanger = setInterval(() => {
+          this.setState({color: randomColor().hexString()})
+        }, pulse)
         return axios.post(url, data, config);
       })
       .then(res => {
@@ -95,6 +103,7 @@ class App extends Component {
             fetching: false
           }
         });
+        clearInterval(this.colorChanger)
         responsiveVoice.speak(", , I have an app , I have a" + result + " that is " + data.data.foods[0].nf_calories + "calories", "Hindi Female", {rate: 0.8});
       })
       .catch(res => {
@@ -130,6 +139,7 @@ class App extends Component {
     const { result, nutrients, food } = this.state;
     const width = window.innerWidth;
     const height = window.innerHeight;
+    console.log(this.state.color)
     return (
       <section className="app-container">
         <audio loop autoPlay ref="audio" onLoadedData={(e,i) => {
@@ -145,7 +155,13 @@ class App extends Component {
           screenshotFormat="image/png"
         />
         {food.fetching || nutrients.fetching ? (
-          <img className="captured-image" src={this.state.imageSrc} alt="" />
+          <div className="captured-image" style={{
+            backgroundImage: "url(" + this.state.imageSrc + ")",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundBlendMode: "screen",
+            backgroundColor: this.state.color
+            }} alt="" />
         ) : null}
         <button className="btn-capture" onClick={this.capture}>
           {" "}
